94def8b8e5f7f8389b78c1feea5165f85e9960ff
sofiane1489 <seh.sof@outlook.com>
2023-05-04 21:19:47 -0400

    sehsof

--- src/test/java/us/piit/luma/LoginTest.java
+++ src/test/java/us/piit/luma/LoginTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import us.piit.SetUp;
 
 public class LoginTest extends SetUp {
-    Logger log= LogManager.getLogger(SetUp.class.getName());
+    Logger log= LogManager.getLogger(LoginTest.class.getName());
     @Test
     public void validCred(){
           //website validation
@@ -26,11 +26,66 @@ public class LoginTest extends SetUp {
           log.info("password enter success");
 
          clickOn("#send2");
+         log.info("click on sign in success");
 
+         //check user logged in
          String expectedWelcomeMess="Welcome, accene lost!";
          String actualWelcomeMess=getElementText("//div[@class='panel header']//span[@class='logged-in'][normalize-space()='Welcome, accene lost!']");
          Assert.assertEquals(expectedWelcomeMess,actualWelcomeMess);
          log.info("login success");
     }
 
+    @Test
+    public void invalidEmail(){
+        //website validation
+        String expectedTitle="Home Page";
+        String actualTitle=getCurrentTtile();
+        Assert.assertEquals(expectedTitle,actualTitle);
+        log.info("landed on luma home page success");
+
+        clickOn("body div.page-wrapper header div.panel.wrapper div ul li.authorization-link a");
+        log.info("click on sign in success");
+
+        type("#email","@gmail.com");
+        log.info("email enter success");
+
+        type("#pass","1234Abcd$");
+        log.info("password enter success");
+
+        clickOn("#send2");
+        log.info("click on sign in success");
+
+        //validate error
+        String expectedWelcomeMess="Please enter a valid email address (Ex: johndoe@domain.com).";
+        String actualWelcomeMess=getElementText("#email-error");
+        Assert.assertEquals(expectedWelcomeMess,actualWelcomeMess);
+        log.info("validate error success");
+    }
+
+    @Test
+    public void missingPassword(){
+        //website validation
+        String expectedTitle="Home Page";
+        String actualTitle=getCurrentTtile();
+        Assert.assertEquals(expectedTitle,actualTitle);
+        log.info("landed on luma home page success");
+
+        clickOn("body div.page-wrapper header div.panel.wrapper div ul li.authorization-link a");
+        log.info("click on sign in success");
+
+        type("#email","so@gmail.com");
+        log.info("email enter success");
+
+        type("#pass","");
+        log.info("password enter success");
+
+        clickOn("#send2");
+        log.info("click on sign in success");
+
+        //validate error
+        String expectedWelcomeMess="This is a required field.";
+        String actualWelcomeMess=getElementText("#pass-error");
+        Assert.assertEquals(expectedWelcomeMess,actualWelcomeMess);
+        log.info("validate error success");
+    }
 }
