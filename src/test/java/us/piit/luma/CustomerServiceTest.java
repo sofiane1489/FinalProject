6fb3edc1b40d3b0aa2e12d8022a76c6888bf3f6c
sofiane1489 <seh.sof@outlook.com>
2023-05-07 17:47:48 -0400

    sehsof

---
+++ src/test/java/us/piit/luma/CustomerServiceTest.java
@@ -0,0 +1,40 @@
+package us.piit.luma;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import us.piit.SetUp;
+
+public class CustomerServiceTest extends SetUp {
+    Logger log= LogManager.getLogger(CustomerServiceTest.class.getName());
+    @Test
+    public void sendEmail(){
+        // website validation
+        String expectedTitle="Home Page";
+        String actualTitle=getCurrentTtile();
+        Assert.assertEquals(expectedTitle,actualTitle);
+        log.info("landed on luma home page success");
+
+        clickOn("//a[text()='Contact Us']");
+        log.info("click on contact us success");
+
+        type("#name","mike lee");
+        log.info("type Name success");
+        type("#email","Betta@gmail.com");
+        log.info("type Email address success");
+        type("#telephone","0123456789");
+        log.info("type telephone success");
+        type("#comment","order not received");
+        log.info("type text in the box success");
+        clickOn(".action.submit.primary");
+        log.info("click on submit success");
+
+        String expectedMessage="Thanks for contacting us with your comments and questions. We'll respond to you very soon.";
+        String actualMessage=getElementText(".message-success.success.message");
+        Assert.assertEquals(actualMessage,expectedMessage);
+        log.info("Email sent success");
+
+    }
+
+}
--- src/test/java/us/piit/luma/SearchBarTest.java
+++ src/test/java/us/piit/luma/SearchBarTest.java
@@ -1,6 +1,7 @@
 package us.piit.luma;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import us.piit.SetUp;
@@ -20,9 +21,10 @@ public class SearchBarTest extends SetUp {
               log.info("type and hit enter text success");
 
         //search result validation
-        boolean expectedPageWraperTitle=true;
-        boolean actualPageWraperTitle=isVisible("#maincontent div h1 span");
+        String expectedPageWraperTitle="Search results for: 'bag pack for man'";
+        String actualPageWraperTitle=getElementText("#maincontent div h1 span");
         Assert.assertEquals(actualPageWraperTitle,expectedPageWraperTitle);
              log.info("serach bar result success");
        }
+
 }
