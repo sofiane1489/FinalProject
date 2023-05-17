7d2e8e4da511ae5f296d2aecfe7a1a32a015e020
sofiane1489 <127370308+sofiane1489@users.noreply.github.com>
2023-05-17 00:11:53 -0400

    sehsof

--- src/test/java/us/piit/automation/CheckOutConfirme.java
+++ src/test/java/us/piit/automation/CheckOutConfirme.java
@@ -1,67 +1,74 @@
-//package us.piit.automation;
-//
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//import us.piit.base.CommonAPI;
-//
-//public class CheckOutConfirme extends CommonAPI {
-//    Logger log = LogManager.getLogger(CheckOutConfirme.class.getName());
-//
-//    @Test
-//    public void checkout() {
-//
-//        //Enter Email, password, and click on login button
-//        clickOn("//header/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[4]/a[1]");
-//        type("//input[@type='email']", "adam20023@outlook.com");
-//        log.info("enter email success");
-//        type("//body/section[@id='form']/div[1]/div[1]/div[1]/div[1]/form[1]/input[3]", "Adana2023#");
-//        log.info("enter password success");
-//        clickOn("//button[contains(text(),'Login')]");
-//        log.info("click on login button Success");
-//        waitFor(1);
-//
-//        // Check user is logged in
-//        String expectedHomePageHeader = "Full-Fledged practice website for Automation Engineers";
-//        String actualHomePageHeader = getElementText("div.container div.row div.col-sm-12 div.carousel.slide div.carousel-inner div.item.active:nth-child(1) div.col-sm-6:nth-child(1) > h2:nth-child(2)");
-//        Assert.assertEquals(expectedHomePageHeader, actualHomePageHeader);
-//        log.info("user logged in success");
-//
-//        //click on cart,proceed to checkout,place order and confirm
-//        clickOn("//header/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[3]/a[1]/i[1]");
-//        log.info("click on shooping cart success");
-//
-//        clickOn("//a[contains(text(),'Proceed To Checkout')]");
-//        log.info("checkout proceed success");
-//
-//        clickOn("//a[contains(text(),'Place Order')]");
-//        log.info("place order success");
-//
-//        type("//body/section[@id='cart_items']/div[1]/div[3]/div[1]/div[2]/form[1]/div[1]/div[1]/input[1]","adam izem");
-//        log.info("type name success");
-//
-//        type(".form-control.card-number","123456789");
-//        log.info("type card number success");
-//
-//        type(".form-control.card-cvc","098");
-//        log.info("cvs enter success");
-//
-//        type(".form-control.card-expiry-month","11");
-//        log.info("expiry-month enter success");
-//
-//        type(".form-control.card-expiry-year","2027");
-//        log.info("expiry-year enter success");
-//
-//        clickOn("#submit");
-//        log.info("pay and confirm order success");
-//
-//
-//        // validate the payment of the product
-//        String expectedtext="Congratulations! Your order has been confirmed!";
-//        String actualtext=getElementText("//p[contains(text(),'Congratulations! Your order has been confirmed!')]");
-//        Assert.assertEquals(expectedtext,actualtext);
-//        log.info("order pay and confirm success");
-//
-//    }
-//}
+package us.piit.automation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import us.piit.automationPages.*;
+import us.piit.base.CommonAPI;
+import us.piit.utility.Utility;
+
+import java.util.Properties;
+
+public class CheckOutConfirme extends CommonAPI {
+    Logger log = LogManager.getLogger(CheckOutConfirme.class.getName());
+    Properties pop= Utility.loadProperties();
+    String validEmail =Utility.decode(pop.getProperty("automation.validEmail"));
+    String validPassword = Utility.decode(pop.getProperty("automation.validPassword"));
+    String name= Utility.decode(pop.getProperty("automation.name"));
+    String cardNumber=Utility.decode(pop.getProperty("automation.cardNumber"));
+    String cvcNumber= Utility.decode(pop.getProperty("automation.cvcNumber"));
+    String expireMonth= Utility.decode(pop.getProperty("automation.expireMonth"));
+    String expireYear= Utility.decode(pop.getProperty("automation.expireYear"));
+    @Test
+    public void checkout() {
+
+
+        //Enter Email, password, and click on login button
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        ViewcartPage viewcartPage=new ViewcartPage(getDriver());
+        ProductsPage productsPage=new ProductsPage(getDriver());
+        CheckOutPage checkoutPage=new CheckOutPage(getDriver());
+        PaymentPage paymentPage=new PaymentPage(getDriver());
+
+        // Verify the title of the page
+        String expectedTitle = "Automation Exercise";
+        String actualTitle = getCurrentTtile();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        waitFor(5);
+
+        // Enter Email, password, and click on login button
+        loginPage.clickonloginbutton();
+        loginPage.enterEmail(validEmail);
+        loginPage.enterPassword(validPassword);
+        loginPage.clickonLoginBtn();
+        waitFor(5);
+
+        // Check if user is logged in
+        String expectedHomePageHeader = "Full-Fledged practice website for Automation Engineers";
+        String actualHomePageHeader = homePage.getHeaderText();
+        Assert.assertEquals(expectedHomePageHeader, actualHomePageHeader);
+
+        //click on cart,proceed to checkout,place order and confirm
+        productsPage.viewProduct();
+        productsPage.addToCart();
+        productsPage.viewCart();
+        checkoutPage.clickCheckOut();
+        paymentPage.clickonPlaceOrder();
+
+        paymentPage.enterName(name);
+        paymentPage.enterCardNumber(cardNumber);
+        paymentPage.entercvcNumber(cvcNumber);
+        paymentPage.enterexpireMonth(expireMonth);
+        paymentPage.enterexpireYear(expireYear);
+        paymentPage.clickonpaymentConfirm();
+
+        // validate the payment of the product
+        String expectedtext="Congratulations! Your order has been confirmed!";
+        String actualtext=homePage.welcomingMessage();
+        Assert.assertEquals(expectedtext,actualtext);
+
+
+    }
+}
